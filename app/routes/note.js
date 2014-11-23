f95b44af03a78dc0be04039441aa520a8ea05349
Steve Kinney <hello@stevekinney.net>
2014-11-23 08:57:55 -0700

    Fix routes again

--- app/routes/note.js
+++ app/routes/note.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function (params) {
-    return this.modelFor('notes').filterBy('id', params.note_id)[0];
+    debugger;
+    return this.modelFor('notes').filterBy('id', parseInt(params.note_id, 10))[0];
   }
 });
