6beabd56ccbab4360a6dcd089f9e384b4fa481b6
Steve Kinney <hello@stevekinney.net>
2014-11-23 09:43:02 -0700

    Don't push debugger statements; add substates

--- app/routes/note.js
+++ app/routes/note.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function (params) {
-    debugger;
     return this.modelFor('notes').filterBy('id', parseInt(params.note_id, 10))[0];
   }
 });
