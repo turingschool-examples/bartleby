d4a78145c2f81095e5558890e3d6013d1327ec33
Steve Kinney <hello@stevekinney.net>
2014-11-25 22:06:57 -0700

    Replace note route model fetching with Ember Data

--- app/routes/note.js
+++ app/routes/note.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function (params) {
-    return this.modelFor('notes').filterBy('id', params.note_id)[0];
+    return this.store.find('note', params.note_id);
   }
 });
